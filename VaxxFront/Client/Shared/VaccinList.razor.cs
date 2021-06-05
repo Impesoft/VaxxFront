aa52a0c11cda9301d661d116c2e4d5e3d1a1329b
Impesoft <mrblubke@yahoo.com>
2021-06-05 12:09:52 +0200

    bug fixes

---
+++ VaxxFront/Client/Shared/VaccinList.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using VaxxFront.Client.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace VaxxFront.Client.Shared
+{
+    public partial class VaccinList
+    {
+
+        private async Task UpdateList()
+        {
+            Console.WriteLine("Update List Requested");
+            Vaccines = await Http.GetFromJsonAsync<List<Vaccine>>("https://localhost:44317/api/Vaccine/" + ((VaccineBodyPart != "All") ? "ForBodyPart/" + _vaccinBodyPart : ""));
+            StateHasChanged();
+            //  Vaccines = Vaccines.Where(x => x.BodyPart == VaccineBodyPart).ToList();
+        }
+        protected override void OnParametersSet()
+        {
+            if (_VaccinBodyPart != VaccineBodyPart)
+            {
+                _VaccinBodyPart = VaccineBodyPart;
+            }
+        }
+    }
+
+}
+
